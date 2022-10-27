1e88e2b4065c2254fc839cfd86dd9948f13dd747
Lidia Mazniku <lidiamazniku@gmail.com>
2022-10-28 00:15:41 +0200

    Fix on Main Activity - adding the right input package name

--- app/src/main/java/com/example/dancway/view/MainActivity.java
+++ app/src/main/java/com/example/dancway/view/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.dancway.R;
+import com.example.danceawaylayout.R;
 
 public class MainActivity extends AppCompatActivity {
 

ab57bcf32b800e8a521519722c21f904d5977b24
Svet <svetoslav.chovalinov@gmail.com>
2022-10-27 14:21:33 +0200

    Added architecture layers

--- app/src/main/java/com/example/dancway/view/MainActivity.java
+++ app/src/main/java/com/example/dancway/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.dancway;
+package com.example.dancway.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.dancway.R;
 
 public class MainActivity extends AppCompatActivity {
 

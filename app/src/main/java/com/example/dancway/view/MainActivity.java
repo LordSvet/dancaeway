6a665e81392f5eb39f3b5609ab1f71b1d87b192e
Lidia Mazniku <lidiamazniku@gmail.com>
2022-10-29 18:12:02 +0200

    Added Main Activity and Main View

--- app/src/main/java/com/example/dancway/view/MainActivity.java
+++ app/src/main/java/com/example/dancway/view/MainActivity.java
@@ -3,14 +3,30 @@ package com.example.dancway.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.example.danceawaylayout.R;
+import com.example.dancway.R;
 
 public class MainActivity extends AppCompatActivity {
+    TextView welcomeText;
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        welcomeText = (TextView) findViewById(R.id.welcomeText);
+        playButton = (Button) findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                welcomeText.setText("Song is now playing");
+            }
+        });
+
     }
 }

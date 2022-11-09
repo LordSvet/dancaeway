ed076858e24c0dca20dfb8363abf5f0e6b124bcd
Halil Berisha <halilberishaa@gmail.com>
2022-11-09 15:55:10 +0100

    Not much, just a text view to test the functionality of Login -> Needs polishing from front-end team

--- app/src/main/java/com/example/dancway/model/PartyGuest.java
+++ app/src/main/java/com/example/dancway/model/PartyGuest.java
@@ -1,5 +1,17 @@
 package com.example.dancway.model;
 
-public class PartyGuest {
+import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dancway.R;
+
+public class PartyGuest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.party_guest_layout);
+    }
 }

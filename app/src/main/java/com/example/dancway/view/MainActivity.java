8727cb223479e2116858bb8560be32039fd34cd1
Halil Berisha <halilberishaa@gmail.com>
2022-11-09 15:54:30 +0100

    Added two buttons for Login and Register in order to implement / test the functionality (simple but working) -> Needs polishing from front-end team

--- app/src/main/java/com/example/dancway/view/MainActivity.java
+++ app/src/main/java/com/example/dancway/view/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.dancway.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.dancway.R;
+import com.example.dancway.model.Login;
+import com.example.dancway.model.Register;
 
 public class MainActivity extends AppCompatActivity {
     TextView welcomeText;
     Button playButton;
+    Button registerButton;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +25,30 @@ public class MainActivity extends AppCompatActivity {
 
         welcomeText = (TextView) findViewById(R.id.welcomeText);
         playButton = (Button) findViewById(R.id.playButton);
+        registerButton = (Button) findViewById(R.id.registerButton);
+        loginButton = (Button) findViewById(R.id.loginButton);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 welcomeText.setText("Song is now playing");
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
             }
         });
 

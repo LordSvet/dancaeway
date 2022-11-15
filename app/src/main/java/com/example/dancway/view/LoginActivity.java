64e79ff4f73e560db8e6cf13d3bc7363d1979654
Halil Berisha <halilberishaa@gmail.com>
2022-11-15 15:54:14 +0100

    Added the implementation of "Forgot you Password"

--- app/src/main/java/com/example/dancway/view/LoginActivity.java
+++ app/src/main/java/com/example/dancway/view/LoginActivity.java
@@ -2,9 +2,11 @@ package com.example.dancway.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,18 +14,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dancway.R;
 import com.example.dancway.controller.UserController;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText editTextEmail, editTextPassword;
     private Button signIn;
-    private TextView signup;
+    private TextView signup, forgotPassword;
     private UserController userController;
+    private ImageView signGoogle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Google Sign thingie
+        createRequest();
+
 
         // only to verify if the login session is already saved or not
         if (UserController.getInstance(this).autologin())
@@ -49,6 +57,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         signIn = (Button) findViewById(R.id.signIn);
         signIn.setOnClickListener(this);
+
+        forgotPassword = (TextView) findViewById(R.id.forgotPassword);
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, ChangePasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signGoogle = (ImageView) findViewById(R.id.googleSign);
+    }
+
+    private void createRequest() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
     }
 
     @Override
@@ -62,8 +89,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             return;
         }
 
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
         if(password.isEmpty()) {
             editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6) {
+            editTextPassword.setError("Min password length is 6 characters!");
             editTextPassword.requestFocus();
             return;
         }

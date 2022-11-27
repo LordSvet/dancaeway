5d5413575af966d543fe210fdf066e7aa2b10529
Maged-10 <75185708+Maged-10@users.noreply.github.com>
2022-11-27 22:22:16 +0100

    Documentation and improvement to the SongQueue class

--- app/src/main/java/com/example/dancway/model/SongQueue.java
+++ app/src/main/java/com/example/dancway/model/SongQueue.java
@@ -1,40 +1,57 @@
 package com.example.dancway.model;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class SongQueue {
 
     private PriorityQueue<Song> queue;
-    private int initialCapacity;
 
+    /**
+     * Create a new song queue with the specified capacity
+     *
+     * @param initialCapacity capacity of the queue
+     */
     public SongQueue(int initialCapacity) {
-        this.initialCapacity = initialCapacity;
-        queue = new PriorityQueue<>(initialCapacity, new Comparator<Song>() {
-            @Override
-            public int compare(Song song1, Song song2) {
-                if (song1.getNrOfLikes() < song2.getNrOfLikes()) {
-                    return 1;
-                } else if (song1.getNrOfLikes() > song2.getNrOfLikes()) {
-                    return -1;
-                }
-                return 0;
+        queue = new PriorityQueue<>(initialCapacity, (song1, song2) -> {
+            if (song1.getNrOfLikes() < song2.getNrOfLikes()) {
+                return 1;
+            } else if (song1.getNrOfLikes() > song2.getNrOfLikes()) {
+                return -1;
             }
+            return 0;
         });
     }
 
+    /**
+     * Add new song to the queue
+     *
+     * @param song The Song object to add
+     * @return true if success; else false
+     */
     public boolean addSong(Song song) {
         return queue.offer(song);
     }
 
+    /**
+     * Clear the song queue
+     */
     public void clear() {
         queue.clear();
     }
 
+    /**
+     * Get the song queue
+     *
+     * @return the song queue
+     */
     public PriorityQueue<Song> getQueue() {
         return queue;
     }
 
+    /**
+     * set the current queue to a new queue
+     * @param queue the new queue
+     */
     public void setQueue(PriorityQueue<Song> queue) {
         this.queue = queue;
     }

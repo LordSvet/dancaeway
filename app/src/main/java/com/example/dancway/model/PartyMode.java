3d329a379446b50cee63f7018dc3e66c7c1acaeb
MeltingCrystals <elshevtsov@gmail.com>
2022-11-30 19:13:25 +0100

    PartyMode class allows basic functionality for the Party Session mode: apart from adding and deleting a user, it saves the current Session with an ID into the RealTime datatbase. It cotains the list of Users, List of songs for Users to choose from, and a queue to fill it with the songs for the party. the method for a current session was created which gets a reference to the db, creates a sessionBranch as a map with keys as variables of the PartyMode class and the respective values. The UserList, SongsList and songsQueue get filled into the sessionBranch in the database.

--- app/src/main/java/com/example/dancway/model/PartyMode.java
+++ app/src/main/java/com/example/dancway/model/PartyMode.java
@@ -3,14 +3,77 @@ package com.example.dancway.model;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
+import com.example.dancway.controller.SongsListController;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class PartyMode extends Session {    //Will implement online session later in next increments
 
-    private List<User> connectedUsers = new ArrayList<>();
+    private String codeGenerator ;
+    private List<User> connectedUsers ;
+    private ArrayList<Song> songList;
+    private SongQueue songQueue;
+
+    public PartyMode(){
+        SessionCodeGen gen = new SessionCodeGen();
+        codeGenerator = gen.getCode();
+        connectedUsers = new ArrayList<>();
+        songList = new ArrayList<>();
+        songQueue = new SongQueue(100);
+
+        currentSession();
+    }
+
+    public void currentSession(){
+        //TODO: make sure after the session is ended , we delete the branch
+
+        //make a ref to db and make a ref to the root which has a child named Session
+        DatabaseReference databaseRoot = FirebaseDatabase.getInstance().getReference().child("Session");
+
+        Map<String, Map<String, String>> sessionBranch = new HashMap<>();
+
+        Map<String, String> userList = new HashMap<>();
+        for(int i = 0; i< connectedUsers.size();i++){
+            userList.put(connectedUsers.get(i).getUsername(),"");
+        }
+        sessionBranch.put("UserList",userList);
+
+        Map<String, String> songs = new HashMap<>();
+        for(int i = 0; i< songList.size();i++){
+            songs.put(songList.get(i).getTitle(), songList.get(i).getUrl());
+
+        }
+        sessionBranch.put("SongsList",songs);
+
+        Map<String, String> queue = new HashMap<>();
+
+        Iterator<Song> iterate = songQueue.getQueue().iterator();
+
+        while(iterate.hasNext()){
+            queue.put(songQueue.getQueue().iterator().next().getTitle(),"");
+        }
+
+        sessionBranch.put("SongsQueue",queue);
+
+
+        databaseRoot.child(codeGenerator).setValue(sessionBranch);
+
+    }
+
+    public String getCodeGenerator() {
+        return codeGenerator;
+    }
+
+    public void setCodeGenerator(String codeGenerator) {
+        this.codeGenerator = codeGenerator;
+    }
 
     public List<User> getConnectedUsers(){
         return connectedUsers;
@@ -28,6 +91,8 @@ public class PartyMode extends Session {    //Will implement online session late
     public void saveCurrentSession(){
 //        DatabaseReference sessionRef = FirebaseDatabase.getInstance().getReference().child("Session").child("id").child("list");
 //        sessionRef.setValue("testValue");
-
     }
+
+
+
 }

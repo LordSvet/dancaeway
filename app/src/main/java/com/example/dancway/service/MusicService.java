f97af93d0a56e71d3ca5f8d0500ca1a5144d54a9
Maged-10 <75185708+Maged-10@users.noreply.github.com>
2023-01-02 22:05:57 +0100

    More Extension to MusicService class

--- app/src/main/java/com/example/dancway/service/MusicService.java
+++ app/src/main/java/com/example/dancway/service/MusicService.java
@@ -5,18 +5,35 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.example.dancway.R;
+import com.example.dancway.controller.MusicPlayerControllerSingleton;
+import com.example.dancway.view.SongPlayerActivity;
+
+/**
+ * Music service class
+ */
 public class MusicService extends Service {
+    public static final String ACTION_NEXT = "NEXT";
+    public static final String ACTION_PLAY = "PLAY";
+    public static final String ACTION_PREV = "PREV";
+    MusicPlayerControllerSingleton musicPlayerControllerSingleton;
+    SongPlayerActivity songPlayerActivity;
     private IBinder mBinder = new MyBinder();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        Log.d("Bind", "Successful");
+        Log.d("Bind", "method");
         return mBinder;
     }
 
+    /**
+     * The Binder class
+     */
     public class MyBinder extends Binder {
         public MusicService getService() {
             return MusicService.this;
@@ -25,6 +42,38 @@ public class MusicService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        String actionName = intent.getStringExtra("action_name");
+        if (actionName != null) {
+            switch (actionName) {
+                case ACTION_PLAY:
+                    if (songPlayerActivity != null) {
+                        songPlayerActivity.pausePlay();
+                    }
+                    break;
+                case ACTION_NEXT:
+                    if (songPlayerActivity != null) {
+                        songPlayerActivity.next();
+                    }
+                    break;
+                case ACTION_PREV:
+                    if (songPlayerActivity != null) {
+                        songPlayerActivity.previous();
+                    }
+                    break;
+            }
+        }
+        return START_STICKY;
     }
+
+    /**
+     * Initialization of dependencies of service
+     *
+     * @param musicPlayerControllerSingleton musicPlayerControllerSingleton
+     * @param songPlayerActivity             songPlayerActivity
+     */
+    public void setCallBack(MusicPlayerControllerSingleton musicPlayerControllerSingleton, SongPlayerActivity songPlayerActivity) {
+        this.musicPlayerControllerSingleton = musicPlayerControllerSingleton;
+        this.songPlayerActivity = songPlayerActivity;
+    }
+
 }
